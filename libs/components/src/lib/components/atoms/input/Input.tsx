b8cd5213dee43a3ed9a69510b9be665e44f6c531
Petyo.Petrov <petyopetrov@Petyos-MacBook-Pro.local>
2023-08-29 11:40:32 +0300

    add scss modules and next app

--- libs/components/src/lib/components/atoms/input/Input.tsx
+++ libs/components/src/lib/components/atoms/input/Input.tsx
@@ -1,9 +1,7 @@
-import styled from 'styled-components';
+import { InputHTMLAttributes } from 'react'
+import styles from './input.module.scss'
 
-export const Input = styled.input`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        
-`;
+export const Input = (props:React.FC<React.InputHTMLAttributes<HTMLInputElement>>)=>{
+
+  return <input className={styles.input} {...props}></input>
+}
